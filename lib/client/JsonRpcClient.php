69428bdbd7d79df329f64c9a0b66a56239599507
Pedro Antonio Fructuoso Merino <pedro.fructuoso@cocomore.com>
2014-08-04 14:35:56 +0200

    Adding support to setup timeout

--- lib/client/JsonRpcClient.php
+++ lib/client/JsonRpcClient.php
@@ -10,14 +10,49 @@ function __autoload($className) {
 class JsonRpcClient {
 	private $_rpcUrl;
 	private $_curlCookie;
+	private $_replyTimeout;
+	private $_connectTimeout;
 
-	public function __construct($rpcUrl) {
+	public function __construct($rpcUrl, $connectTimeout = 1, $replyTimeout = 1) {
 		$this->_rpcUrl = $rpcUrl;
 		$this->_curlCookie = dirname(__FILE__).DIRECTORY_SEPARATOR.'curl_cookie';
+        $this->_replyTimeout = $replyTimeout;
+        $this->_connectTimeout = $connectTimeout;
 	}
 	public function __call($name, $arguments) {
 		return $this->call(new RpcRequest($name,$arguments));
 	}
+
+    /**
+     * Set reply timeout in seconds.
+     * @param $secs
+     */
+    public function setReplyTimeout($secs) {
+        $this->_replyTimeout = $secs;
+    }
+
+    /**
+     * Get reply timeout in seconds.
+     */
+    public function getReplyTimeout() {
+        return $this->_replyTimeout;
+    }
+
+    /**
+     * Set connect timeout in seconds.
+     * @param $secs
+     */
+    public function setConnectTimeout($secs) {
+        $this->_connectTimeout = $secs;
+    }
+
+    /**
+     * Get connect timeout in seconds.
+     */
+    public function getConnectTimeout() {
+        return $this->_connectTimeout;
+    }
+
 	public function call($rpcRequest) {
 		if($rpcRequest instanceof RpcRequest) {
 			return $this->httpRequest($rpcRequest->getRpcRequestObject());
@@ -36,6 +71,9 @@ class JsonRpcClient {
 		$curlHandler = curl_init();
 		$curlOptions = array(
 			CURLOPT_URL => $this->_rpcUrl,
+            // Timeout
+            CURLOPT_TIMEOUT => $this->_replyTimeout,
+            CURLOPT_CONNECTTIMEOUT => $this->_connectTimeout,
 			// cookie stuff
 			CURLOPT_COOKIE => true,
             CURLOPT_COOKIEFILE => $this->_curlCookie,
@@ -45,7 +83,6 @@ class JsonRpcClient {
 			CURLOPT_POST => true,
 			CURLOPT_POSTFIELDS => json_encode($rpcBatchArray),
 			CURLOPT_RETURNTRANSFER => true
-
 		);
 		curl_setopt_array($curlHandler,$curlOptions);
 
